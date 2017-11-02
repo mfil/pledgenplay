15da477c1f6c0b635406d2b6a209274d26dafb72
Max Fillinger <Max.Fillinger@cwi.nl>
2017-11-02 04:03:57 +0100

    Consistency

--- test/test_input_file.c
+++ test/test_input_file.c
@@ -64,7 +64,7 @@ seek_and_check(long offset)
 	ck_assert_int_eq(status, SEEK_OK);
 }
 
-START_TEST(input_file_determines_filetype)
+START_TEST (input_file_determines_filetype)
 {
 	char *testfiles[3] = {"testdata/test.flac", "testdata/test.mp3",
 	    "testdata/test.wav"};
@@ -81,7 +81,7 @@ START_TEST(input_file_determines_filetype)
 }
 END_TEST
 
-START_TEST(input_file_detects_id3v2_tags)
+START_TEST (input_file_detects_id3v2_tags)
 {
 	char *testfiles[3] = {"testdata/with_id3v2.flac",
 	    "testdata/with_id3v2.mp3", "testdata/with_id3v2.wav"};
@@ -98,7 +98,7 @@ START_TEST(input_file_detects_id3v2_tags)
 }
 END_TEST
 
-START_TEST(input_file_calls_file_err_on_invalid_file)
+START_TEST (input_file_calls_file_err_on_invalid_file)
 {
 	int fd = open("testdata/random_garbage", O_RDONLY);
 	if (fd < 0) {
@@ -112,7 +112,7 @@ START_TEST(input_file_calls_file_err_on_invalid_file)
 }
 END_TEST
 
-START_TEST(input_file_read_and_seek_works)
+START_TEST (input_file_read_and_seek_works)
 {
 	const size_t TEST_BUF_SIZE = 100;
 	int fd = open("testdata/test.flac", O_RDONLY);
@@ -146,6 +146,38 @@ START_TEST(input_file_read_and_seek_works)
 }
 END_TEST
 
+START_TEST (no_read_after_closing)
+{
+	int fd = open("testdata/test.flac", O_RDONLY);
+	if (fd < 0) {
+		err(1, "open");
+	}
+	set_new_file_and_check(fd);
+
+	close_and_check();
+
+	READ_STATUS status = input_file_read(NULL, 0, NULL);
+	ck_assert_int_eq(status, READ_NO_FILE);
+}
+END_TEST
+
+START_TEST (no_seek_after_closing)
+{
+	int fd = open("testdata/test.flac", O_RDONLY);
+	if (fd < 0) {
+		err(1, "open");
+	}
+	set_new_file_and_check(fd);
+
+	close_and_check();
+
+	SEEK_STATUS status = input_file_rewind();
+	ck_assert_int_eq(status, SEEK_NO_FILE);
+	status = input_file_seek(0);
+	ck_assert_int_eq(status, SEEK_NO_FILE);
+}
+END_TEST
+
 Suite
 *child_messages_suite(void)
 {
@@ -158,6 +190,8 @@ Suite
 
 	TCase *tc_read_seek = tcase_create("read and seek"); 
 	tcase_add_test(tc_read_seek, input_file_read_and_seek_works);
+	tcase_add_test(tc_read_seek, no_read_after_closing);
+	tcase_add_test(tc_read_seek, no_seek_after_closing);
 
 	suite_add_tcase(s, tc_filetype);
 	suite_add_tcase(s, tc_read_seek);
